3d7bcdac580087203c47cc676f2c6a58583da14c
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2026-06-10 15:29:49 +0200

    Enable debug overlay and refine orbit limits

--- app/src/app/(landingPage)/components/Scene/controls/orbitProfiles.js
+++ app/src/app/(landingPage)/components/Scene/controls/orbitProfiles.js
@@ -24,13 +24,13 @@ const MODEL_PROFILES = {
   "/assets/models/14/14-optimized.glb": {
     limits: {
       minAzimuth: -166.0,
-      maxAzimuth: -38.0,
-      minOrbitY: 4.7,
-      maxOrbitY: 90.0,
+      maxAzimuth: -62.3,
+      minOrbitY: 51.0,
+      maxOrbitY: 74.5,
     },
     initial: {
-      azimuth: 76.9,
-      orbitY: 7.2,
+      azimuth: -133.3,
+      orbitY: 74.5,
     },
   },
   "/assets/models/16/16-optimized.glb": {
